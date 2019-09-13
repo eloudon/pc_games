ce520c37e0f682bc7dfe137c726f6bb3e78178d3
Eric Loudon <loudon.eric@gmail.com>
2019-09-13 11:28:50 +0000

    describe and exit

--- lib/pc_games/cli.rb
+++ lib/pc_games/cli.rb
@@ -3,6 +3,8 @@ class PcGames::CLI
   def call
     puts "Techradar's Top 3 PC games are:"
     top_three
+    describe
+    goodbye
   end
 
   def top_three
@@ -13,5 +15,24 @@ class PcGames::CLI
     DOC
   end
 
-
+def describe
+  puts "Select a game you would like to hear more about by entering it's number:"
+  input = nil
+  while input != "exit"
+  input = gets.strip
+  case input
+when "1"
+  puts "describe the witcher"
+when "2"
+  puts "describe dark souls"
+when "3"
+  puts "describe MHW"
+  end
+ end
 end
+
+def goodbye
+  puts "Don't forget to check in to see when rankings change!"
+ end
+
+ end
