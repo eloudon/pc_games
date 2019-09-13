e5886bda7c624257e3a4476dbe32b6f2ae2e8e82
Eric Loudon <loudon.eric@gmail.com>
2019-09-13 11:13:22 +0000

    requires

--- lib/pc_games.rb
+++ lib/pc_games.rb
@@ -1,6 +1,2 @@
-require "pc_games/version"
-
-module PcGames
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require_relative "pc_games/version"
+require_relative "./pc_games/cli"
--- lib/pc_games/cli.rb
+++ lib/pc_games/cli.rb
@@ -1,7 +1,17 @@
 class PcGames::CLI
 
   def call
-    puts "Techradar's Top 5 PC games are:"
+    puts "Techradar's Top 3 PC games are:"
+    top_three
   end
+
+  def top_three
+    puts <<-DOC
+    1. The Witcher 3
+    2. Dark Souls 3
+    3. Monster Hunter World
+    DOC
+  end
+
 
 end
